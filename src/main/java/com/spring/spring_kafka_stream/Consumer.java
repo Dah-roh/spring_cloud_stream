133534e5ec1c9c5171d1a9e9b1c5910041d56677
dizsart <darogadibia@gmail.com>
2020-06-17 13:41:16 +0100

    add the handle method to the Consumer class

--- src/main/java/com/spring/spring_kafka_stream/Consumer.java
+++ src/main/java/com/spring/spring_kafka_stream/Consumer.java
@@ -1,10 +1,17 @@
 package com.spring.spring_kafka_stream;
 
+import com.spring.spring_kafka_stream.domain.ChatMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 @EnableBinding(Sink.class)
 public class Consumer {
@@ -18,4 +25,12 @@ public class Consumer {
         logger.info("recieved a string message : " + message);
     }
 
+    @StreamListener(target = Sink.INPUT, condition = "headers['type']=='chat'")
+    public void handle(@Payload ChatMessage message) {
+
+        final DateTimeFormatter df = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)
+                .withZone(ZoneId.systemDefault());
+        final String time = df.format(Instant.ofEpochMilli(message.getTime()));
+        logger.info("recieved a complex message : [{}]: {}", time, message.getContents());
+    }
 }
