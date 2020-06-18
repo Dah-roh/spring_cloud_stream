7189f2069aa292f1b43c01368501609a3fd83208
dizsart <darogadibia@gmail.com>
2020-06-18 13:33:22 +0100

    add the producer and consumer configuration and the controllers

---
+++ src/main/java/com/spring/spring_kafka_stream/Producer.java
@@ -0,0 +1,28 @@
+package com.spring.spring_kafka_stream;
+
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+
+@EnableBinding(Source.class)
+public class Producer {
+
+    private Source mySource;
+
+    public Producer(Source mySource) {
+
+        super();
+        this.mySource = mySource;
+    }
+
+    public Source getMysource() {
+
+        return mySource;
+    }
+
+    public void setMysource(Source mysource) {
+
+        mySource = mysource;
+    }
+
+}
